ee54d9e886c296ed2196817eb2c0db88f327d4dd
Joshtri <stuffofyos1516@gmail.com>
2024-09-07 11:05:39 +0800

    uodatete

--- controllers/kegiatan.controller.js
+++ controllers/kegiatan.controller.js
@@ -1,17 +1,25 @@
 /* eslint-disable no-useless-catch */
 import * as kegiatanServices from '../services/kegiatan.services.js';
+import { ref, getDownloadURL, uploadBytesResumable, getStorage } from "firebase/storage"; // Pastikan ini sudah di-import
+import { signInWithEmailAndPassword } from "firebase/auth"; // Firebase Auth
+import Kegiatan from '../models/kegiatan.model.js'; // Import model Kegiatan
+import { auth } from '../config/firebaseConfig.js'; // Sesuaikan dengan path modul Anda
+import { config } from 'dotenv';
+config();
 
-import Kegiatan from '../models/kegiatan.model.js';
 
-export const kegiatanPage = async(req,res)=>{
-    const title = "Data Kegiatan"
+export const kegiatanPage = async (req, res) => {
+    const title = "Data Kegiatan";
     try {
-        
         const messageCreateSuccess = await req.flash('messageCreateSuccess');
         const messageDeleteSuccess = await req.flash('messageDeleteSuccess');
         const messageUpdateSuccess = await req.flash('messageUpdateSuccess');
-        const kegiatanData = await kegiatanServices.getAllKegiatan();
-        res.render('data_kegiatan',{
+        const kegiatanData = await Kegiatan.find();
+
+        // Pastikan data kegiatan diambil dengan benar
+        // console.log('Kegiatan Data:', kegiatanData);
+
+        res.render('data_kegiatan', {
             title,
             kegiatanData,
             messageCreateSuccess,
@@ -19,104 +27,90 @@ export const kegiatanPage = async(req,res)=>{
             messageUpdateSuccess
         });
     } catch (error) {
+        console.error('Error fetching kegiatan data:', error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+
+
+// Fungsi untuk mengunggah file gambar ke Firebase Storage dan menyimpan data teks ke MongoDB
+async function uploadSingleImage(file, kegiatanData) {
+    try {
+        const pass = process.env.FIREBASE_PASS;
+        const user = process.env.FIREBASE_USER;
+
+        // Sign in to Firebase
+        await signInWithEmailAndPassword(auth, user, pass);
+
+        const dateTime = Date.now();
+        let uploadedFileName = null;
+
+        // Validasi dan upload file jika ada
+        if (file) {
+            // Validasi apakah file adalah gambar
+            const validImageTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+            if (!validImageTypes.includes(file.mimetype)) {
+                throw new Error('Invalid file type. Only JPEG, PNG, and JPG are allowed.');
+            }
+
+            const fileName = `images/${dateTime}_${file.originalname}`;
+            const storageRef = ref(getStorage(), fileName);
+            const metadata = { contentType: file.mimetype }; // Metadata untuk gambar
+
+            const uploadTask = await uploadBytesResumable(storageRef, file.buffer, metadata);
+
+            // Mendapatkan URL dari file yang di-upload
+            uploadedFileName = await getDownloadURL(uploadTask.ref);
+        }
+
+        // Save kegiatan to MongoDB
+        const newKegiatan = new Kegiatan({
+            nama_kegiatan: kegiatanData.nama_kegiatan,
+            tanggal_kegiatan: kegiatanData.tanggal_kegiatan,
+            deskripsi_kegiatan: kegiatanData.deskripsi_kegiatan,
+            foto_kegiatan: uploadedFileName, // Simpan URL gambar
+        });
+
+        await newKegiatan.save();
+        return uploadedFileName;
+    } catch (error) {
+        console.error("Error uploading file:", error);
         throw error;
     }
+}
 
-};
-
-// Controller to update kegiatan
-export const updateKegiatanPage = async (req, res) => {
-    const { id } = req.params;
-    let title = "Edit Kegiatan";
-    try {
-        const kegiatanEdit = await Kegiatan.findById(id)
-        if (!kegiatanEdit) {
-          return res.status(404).json({ message: 'Kegiatan not found' });
-        }
-
-        res.render('edit_kegiatan',{
-            title,
-            kegiatanEdit
-            
-
-        });
-    } catch (error) {
-        console.log(error);
-    }
-};
-
-// Controller untuk mengupdate data kegiatan
-export const updateDataKegiatan = async (req, res) => {
-    const { id } = req.params;
-    const { nama_kegiatan, tanggal_kegiatan_old, tanggal_kegiatan_new, deskripsi_kegiatan } = req.body;
-
-    try {
-        let updatedKegiatan;
-        
-        // Periksa apakah ada perubahan tanggal_kegiatan
-        if (tanggal_kegiatan_new) {
-            updatedKegiatan = await Kegiatan.findByIdAndUpdate(
-                id,
-                { nama_kegiatan, tanggal_kegiatan: tanggal_kegiatan_new, deskripsi_kegiatan },
-                { new: true }
-            );
-        } else {
-            updatedKegiatan = await Kegiatan.findByIdAndUpdate(
-                id,
-                { nama_kegiatan, tanggal_kegiatan: tanggal_kegiatan_old, deskripsi_kegiatan },
-                { new: true }
-            );
-        }
-
-        if (!updatedKegiatan) {
-            return res.status(404).json({ message: 'Kegiatan tidak ditemukan' });
-        }
-
-        req.flash('messageUpdateSuccess', 'Data kegiatan berhasil diupdate.');
-        res.redirect('/adm/data/kegiatan');
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Terjadi kesalahan saat memperbarui kegiatan' });
-    }
-};
-
-
-
-
-
-// Create a new kegiatan
 export const createKegiatan = async (req, res) => {
     try {
-        const { nama_kegiatan, tanggal_kegiatan, deskripsi_kegiatan, foto_kegiatan } = req.body;
+        // Validate required fields
+        const requiredFields = ['nama_kegiatan', 'tanggal_kegiatan', 'deskripsi_kegiatan'];
+        for (const field of requiredFields) {
+            if (!req.body[field]) {
+                return res.status(400).send(`Field '${field}' is required`);
+            }
+        }
 
-        const kegiatan = new Kegiatan({
-            nama_kegiatan,
-            tanggal_kegiatan,
-            deskripsi_kegiatan,
-            foto_kegiatan
-        });
+        const kegiatanData = {
+            nama_kegiatan: req.body.nama_kegiatan,
+            tanggal_kegiatan: req.body.tanggal_kegiatan,
+            deskripsi_kegiatan: req.body.deskripsi_kegiatan,
+        };
 
-        await kegiatan.save();
-        res.status(201).send(kegiatan);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-};
+        // Call the upload function for images
+        const uploadedFileName = await uploadSingleImage(req.file, kegiatanData);
 
-export const deleteKegiatan = async (req, res, next) => {
-    const { id } = req.params;
+        req.flash('successUpHse', 'Data Kegiatan berhasil di upload');
 
-    try {
+        console.log(`Upload success, file name: ${uploadedFileName}`);
 
-        // Jika tidak ada UMKM yang berelasi, lanjutkan menghapus data penduduk
-        const deletedKegiatan = await kegiatanServices.deleteKegiatan(id);
-
-        await req.flash('messageDeleteSuccess', 'Data Kegiatan berhasil dihapus.');
+        // Redirect or respond
         res.redirect('/adm/data/kegiatan');
     } catch (error) {
-        next(error);
+        console.error("Error:", error);
+        res.status(500).send('Internal Server Error');
     }
 };
+
 
 export const addKegiatanPage = async(req,res)=>{
     const title = "Tambah Kegiatan"
--- routes/kegiatan.route.js
+++ routes/kegiatan.route.js
@@ -2,19 +2,24 @@ import express from "express";
 const kegiatanRoute = express.Router();
 import * as kegiatanController from "../controllers/kegiatan.controller.js";
 import protect from "../config/auth/protect.js";
+import multer from 'multer';
+
+
+// Setup multer untuk menangani file upload
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: { fileSize: 2 * 1024 * 1024 }, // Batasan ukuran file 5MB
+});
+
 
 kegiatanRoute.get('/kegiatan', protect, kegiatanController.kegiatanPage);
-kegiatanRoute.post('/kegiatan', protect, kegiatanController.createKegiatan);
+// kegiatanRoute.post('/kegiatan', protect, kegiatanController.createKegiatan);
 
 kegiatanRoute.get('/add_kegiatan',protect, kegiatanController.addKegiatanPage);
 
-kegiatanRoute.delete('/kegiatan/:id',protect, kegiatanController.deleteKegiatan);
 
-// Route to update kegiatan
-kegiatanRoute.get('/edit_kegiatan/:id',protect, kegiatanController.updateKegiatanPage);
-
-kegiatanRoute.put('/update_kegiatan/:id',protect, kegiatanController.updateDataKegiatan);
-
+// Route untuk submit form tambah kegiatan (POST)
+kegiatanRoute.post('/kegiatan', upload.single('foto_kegiatan'), kegiatanController.createKegiatan); // Menangani file upload dan memproses data
 
 
 export default kegiatanRoute;
